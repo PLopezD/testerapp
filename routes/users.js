da305ca0ac6c3db574efa92d6aeea9b1ab730015
Pablo Lopez Domowicz <pldomowicz@gmail.com>
2015-04-27 11:47:15 -0500

    user login view done, user model created

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,26 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/create', function(req, res, next) {
+	vm = {
+  	title: 'Create an Account'
+  }
+  res.render('users/create',vm);
+});
+
+router.post('/create', function(req, res, next) {
+	var something = false
+	console.log(req.body)
+	if (something){
+	vm = {
+  	title: 'Create an Account',
+  	input: req.body,
+  	error: "Something's up"
+  }
+  delete vm.input.password
+  return res.render('users/create',vm);
+}
+res.redirect('/orders')
+});
 
 module.exports = router;
